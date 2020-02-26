ef646110d9c37dc17d856232a9cafa0b13ab1d47
neulhan <sting7675@naver.com>
2020-02-26 21:32:05 +0900

    2.2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React from 'react';
 
-function Food({fav}) {
-  console.log(fav);
-  return <h1>{fav}</h1>
+function Food({name,image}) {
+  console.log(name, image);
+  return (
+    <div>
+      <h1>{name}</h1>
+      <img src={image}/>
+    </div>
+  )
 }
 
+const foodILike = [
+  {
+    name: "김밥" ,
+    image: "https://s.pstatic.net/static/www/mobile/edit/2017/0426/mobile_183053995499.png"
+  },
+  {
+    name: "삼겹살",
+    image: "https://s.pstatic.net/static/www/mobile/edit/2017/0310/mobile_173318132897.png"
+  },
+  {
+    name: "치킨",
+    image: "https://s.pstatic.net/static/www/mobile/edit/2017/0426/mobile_18432363571.png"
+  }
+];
 
 function App() {
   return (
     <div>
       <div>hello</div>
-      <Food fav={"kimchi"}/>
-      <Food fav={"라면"}/>
-      <Food fav={"삼겹살"}/>
-      <Food fav={"쭈꾸미"}/>
+      {foodILike.map(each => <Food name={each.name} image={each.image}/>)}
     </div>
   );
 }
