534118867233fe56ffc48ae064e8939e55a269ef
neulhan <sting7675@naver.com>
2020-03-07 16:29:18 +0900

    3.2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class App extends React.Component{
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("construct");
+  }
   state = {
     count: 0
   };
   add = () => {
-    this.setState({count: this.state.count+1});
+    this.setState({ count: this.state.count + 1 });
   };
   minus = () => {
-    this.setState(current => ({count: current.count-1}));
+    this.setState(current => ({ count: current.count - 1 }));
   };
-
-  render () {
-  return (
+  componentDidMount(){
+    console.log("componentDidMount");
+  }
+  componentDidUpdate(){
+    console.log("componentDidUpdate");
+  }
+  componentWillUnmount(){
+    console.log("componentWillUnmount");
+  }
+  render() {
+    console.log("render");
+    return (
       <div>
         <h1>The number is {this.state.count}</h1>
         <button onClick={this.add}>add</button>
